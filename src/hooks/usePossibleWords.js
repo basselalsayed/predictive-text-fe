2dca56896d4cdf4331f5fac174598bfdcc0081e6
basselalsayed <alsayed.bassel@gmail.com>
2021-08-19 21:54:03 +0100

    useAutoEffect to auto fetch possible words onchange

--- src/hooks/usePossibleWords.js
+++ src/hooks/usePossibleWords.js
@@ -1,6 +1,8 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { postDigits as _postDigits } from '../redux/modules/possibleWords';
+import { useAutoEffect } from './useAutoEffect';
+import useDigits from './useDigits';
 
 const usePossibleWords = () => {
   const dispatch = useDispatch();
@@ -8,10 +10,18 @@ const usePossibleWords = () => {
   const { possibleWords, postingPossibleWords, possibleWordsError } =
     useSelector(({ possibleWords }) => possibleWords, shallowEqual);
 
+  const { digits } = useDigits();
+
   const postDigits = useCallback(
     digits => dispatch(_postDigits(digits)),
     [dispatch],
   );
+
+  useAutoEffect({
+    condition: digits.length,
+    callback: () => postDigits(digits),
+    deps: [digits, postDigits],
+  });
 
   return {
     possibleWords,
