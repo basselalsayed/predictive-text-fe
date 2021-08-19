b94600d63372521aec85d95beabdbe243d384981
basselalsayed <alsayed.bassel@gmail.com>
2021-08-19 17:52:13 +0100

    map over inputs in Keypad.js to create initial grid

--- src/components/Keypad/index.js
+++ src/components/Keypad/index.js
@@ -1,9 +1,35 @@
-import styles from './keypad.scss';
+import styles from './keypad.module.scss';
 
 import React from 'react';
+import InputCircle from '../InputCircle/InputCircle';
 
-const Keypad = () => {
-  return <div></div>;
-};
+const inputs = [
+  [],
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i'],
+  ['j', 'k', 'l'],
+  ['m', 'n', 'o'],
+  ['p', 'q', 'r', 's'],
+  ['t', 'u', 'v'],
+  ['w', 'x', 'y', 'z'],
+];
+
+const Keypad = () => (
+  <div className={styles.container}>
+    {inputs.map((input, index) => (
+      <InputCircle
+        style={{ justifySelf: 'center' }}
+        key={index}
+        onClick={() => {}}
+      >
+        <>
+          <span>{index + 1}</span>
+          <span>{input.join('').toUpperCase()}</span>
+        </>
+      </InputCircle>
+    ))}
+  </div>
+);
 
 export default Keypad;
