7a5486c4b4c10465bc50144e82b52841f8d2eb9a
basselalsayed <alsayed.bassel@gmail.com>
2021-08-19 16:57:09 +0100

    wrap postDigits function (hook) in useCallback

--- src/hooks/usePossibleWords.js
+++ src/hooks/usePossibleWords.js
@@ -1,9 +1,20 @@
+import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { postDigits as _postDigits } from '../redux/modules/possibleWords';
 
 const usePossibleWords = () => {
   const dispatch = useDispatch();
   const { possibleWords, postingPossibleWords, possibleWordsError } =
     useSelector(state => state);
+
+  const postDigits = useCallback(
+    digits => dispatch(_postDigits(digits)),
+    [dispatch],
+  );
+
+  return {
+    postDigits,
+  };
 };
 
 export default usePossibleWords;
